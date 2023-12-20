7b2b53a15e6f0cc24ea2e4b75acf143c7015c43b
Jus Ferrell <j1ferrel@ucsd.edu>
2023-12-20 08:44:21 -0800

    resolved text to shape size ratio, renamed svg folder, new comments, adjustments to tests, more

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+//import packages
 const inquirer = require('inquirer');
 const { writeFile, readdir } = require('fs').promises;
 const path = require('path');
 
+//import classes
 const { Circle, Triangle, Square } = require('./lib/shapes');
 const { SVGGenerator } = require('./lib/svgGenerator');
 
-const examplesDir = path.join('.', 'examples');
+//global concatenation of current dir path w/ svg-files dir
+const svgFileDir = path.join('.', 'svg-files');
 
 //user prompts for SVG specifications
 const userPrompt = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'text',
+            name: 'displayText',
             message: 'Enter up to three characters for the logo:'
         },
         {
@@ -41,22 +44,22 @@ const userPrompt = () => {
 };
 
 /*
-Function called when user does not enter a file name, or if the file name is blank (whitespace).
+Function called when user does not enter a file name, or if the filename is blank (whitespace).
 Each generated SVG file is named in ascending order - 'logo1.svg', 'logo2.svg', etc.
 */
 const setFileName = async () => {
     try {
-        const files = await readdir(examplesDir);
+        const files = await readdir(svgFileDir);
         const logoNumbers = files
             .filter(file => file.startsWith('logo') && file.endsWith('.svg')) //filters filenames that start w/ 'logo' and end w/ '.svg'
             .map(file => parseInt(file.match(/logo(\d+)\.svg/)?.[1] || 0)) //looks for a number in each filename and extracts it for use in sorting
             .sort((a, b) => a - b); //sorts numbers in ascending order
 
-        const nextNumber = logoNumbers.length > 0 ? logoNumbers[logoNumbers.length - 1] + 1 : 1;
-        return path.join(examplesDir, `logo${nextNumber}.svg`);
+        const nextNumber = logoNumbers.length > 0 ? logoNumbers[logoNumbers.length - 1] + 1 : 1; //determines filename for next SVG file to be created
+        return path.join(svgFileDir, `logo${nextNumber}.svg`);
     } catch (err) {
         console.error(err);
-        return path.join(examplesDir, 'logo1.svg'); //in case of an error, defaults to 'logo1.svg' in the examples folder
+        return path.join(svgFileDir, 'logo1.svg'); //in case of an error, defaults to 'logo1.svg' in the examples folder
     }
 };
 
@@ -65,19 +68,18 @@ const main = async () => {
     try {
         const input = await userPrompt();
         //construct shape object from user input
-        let shape;
+        let svgShape;
         switch (input.shape) {
             case 'Circle':
-                shape = new Circle(input.shapeColor);
+                svgShape = new Circle(input.shapeColor);
                 break;
             case 'Triangle':
-                shape = new Triangle(input.shapeColor);
+                svgShape = new Triangle(input.shapeColor);
                 break;
             case 'Square':
-                shape = new Square(input.shapeColor);
+                svgShape = new Square(input.shapeColor);
                 break;
         }
-        const svgGenerator = new SVGGenerator(shape, input.text, input.textColor);
         
         let fileName = input.fileName.trim(); //check if user provided a file name or if their file name was only white space
         if (!fileName) {
@@ -87,9 +89,14 @@ const main = async () => {
             if (!fileName.endsWith('.svg')) {
                 fileName += '.svg';
             }
-            fileName = path.join(examplesDir, fileName);
+            fileName = path.join(svgFileDir, fileName);
         }
 
+        const displayText = JSON.stringify(input.displayText)
+            .toUpperCase() //force uppercase due to hardcoded text positions
+            .replace(/"/g, ''); //remove quotes
+        const svgGenerator = new SVGGenerator(svgShape, displayText, input.textColor);
+        
         await writeFile(fileName, svgGenerator.generateSVG());
         console.log(`Logo generated successfully and written to ${fileName}`);
     } catch (err) {
