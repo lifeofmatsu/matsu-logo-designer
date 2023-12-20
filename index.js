436d4df63d33692740d3c0842b930cb4e720fd7b
Jus Ferrell <j1ferrel@ucsd.edu>
2023-12-20 03:09:19 -0800

    addition of auto-naming svg file if no user input for file name

--- index.js
+++ index.js
@@ -35,9 +35,25 @@ const userPrompt = () => {
 
 };
 
-const main = () => {
-    userPrompt()
-    .then((input) => {
+const getNextFileName = async () => {
+    try {
+        const files = await readdir(examplesDir);
+        const logoNumbers = files
+            .filter(file => file.startsWith('logo') && file.endsWith('.svg')) 
+            .map(file => parseInt(file.match(/logo(\d+)\.svg/)?.[1] || 0)) 
+            .sort((a, b) => a - b); 
+
+        const nextNumber = logoNumbers.length > 0 ? logoNumbers[logoNumbers.length - 1] + 1 : 1;
+        return path.join(examplesDir, `logo${nextNumber}.svg`);
+    } catch (err) {
+        console.error(err);
+        return path.join(examplesDir, 'logo1.svg'); 
+    }
+};
+
+const main = async () => {
+    try {
+        const input = await userPrompt();
         let shape;
         switch (input.shape) {
             case 'Circle':
@@ -53,10 +69,24 @@ const main = () => {
 
         const svgGenerator = new SVGGenerator(shape, input.text, input.textColor);
 
-        writeFile(input.fileName, svgGenerator.generateSVG())
-    })
-    .then(() => console.log('Logo generated successfully and written to logo.svg'))
-    .catch((e) => console.error(e));
-}
+        //check if user provided a file name or if their file name was only white space
+        let fileName = input.fileName.trim();
+
+        if (!fileName) {
+            fileName = await getNextFileName();
+        } else {
+            //ensures the file name is appended with '.svg' file extension
+            if (!fileName.endsWith('.svg')) {
+                fileName += '.svg';
+            }
+            fileName = path.join(examplesDir, fileName);
+        }
+
+        await writeFile(fileName, svgGenerator.generateSVG());
+        console.log(`Logo generated successfully and written to ${fileName}`);
+    } catch (err) {
+        console.error(err);
+    }
+};
 
 main(); //initialize
