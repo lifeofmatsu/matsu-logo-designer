f93ad32d37aa4f03cd594b2c9883feef45f94611
Jus Ferrell <j1ferrel@ucsd.edu>
2023-12-20 03:03:25 -0800

    user prompts written; main func shape selection, svg generation, file writing logic

---
+++ index.js
@@ -0,0 +1,57 @@
+const inquirer = require('inquirer');
+
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const { SVGGenerator } = require('./lib/svgGenerator');
+
+const userPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the logo:'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a text color (keyword or hex):'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter a color for the shape (keyword or hex):'
+        }
+    ]);
+
+};
+
+const main = () => {
+    userPrompt()
+    .then((input) => {
+        let shape;
+        switch (input.shape) {
+            case 'Circle':
+                shape = new Circle(input.shapeColor);
+                break;
+            case 'Triangle':
+                shape = new Triangle(input.shapeColor);
+                break;
+            case 'Square':
+                shape = new Square(input.shapeColor);
+                break;
+        }
+
+        const svgGenerator = new SVGGenerator(shape, input.text, input.textColor);
+
+        writeFile('logo.svg', svgGenerator.generateSVG())
+    })
+    .then(() => console.log('Logo generated successfully and written to logo.svg'))
+    .catch((e) => console.error(e));
+}
+
+main(); //initialize
